bbedd40e57ebd8249164a96109b1de07835533ec
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-06-17 15:20:21 +0530

    code added

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
+const message = "Hello, World!";
+const fileName = 'output.txt';
+
+function writeMessageToFile(message, fileName) {
+    fs.writeFile(fileName, message, (err) => {
+        if (err) {
+            console.error("An error occurred during the file writing process:", err);
+            throw err;
+        }
+        console.log(message);
+    });
+}
+
+writeMessageToFile(message, fileName);
